5545beb159dda8c24c69ee6905240fbd3285b800
Redwi-v <forest45456@gmail.com>
2022-04-18 07:50:58 +0300

    slider

--- src/components/Clothes/Clothes.jsx
+++ src/components/Clothes/Clothes.jsx
@@ -1,4 +1,4 @@
-import img from '../../img/musicians.png';
+import image from '../../img/musicians.png';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -6,36 +6,107 @@ import style from './clothes.module.scss';
 
 const Clothes = props => {
 	const settings = {
-		className: style.center,
 		dots: true,
 		infinite: true,
 		speed: 500,
-		slidesToShow: 3,
+		slidesToShow: 1,
 		slidesToScroll: 1,
-		centerMode: true,
+		nextArrow: <SampleNextArrow />,
+		prevArrow: <SampleNextArrow />,
 	};
 	return (
-		<Slider {...settings}>
-			<div>
-				<h3>1</h3>
-			</div>
-			<div>
-				<h3>2</h3>
-			</div>
-			<div>
-				<h3>3</h3>
-			</div>
-			<div>
-				<h3>4</h3>
-			</div>
-			<div>
-				<h3>5</h3>
-			</div>
-			<div>
-				<h3>6</h3>
-			</div>
-		</Slider>
+		<div className='container'>
+			<Slider {...settings} className={style.slider}>
+				<div>
+					<h1>number one</h1>
+					<div className={style.content}>
+						<img src={image} alt='' />
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti quaerat fugit ea, similique
+							explicabo. Aspernatur nemo qui nostrum, repellat quo delectus deserunt ipsa consectetur distinctio, cum
+							omnis fuga quisquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi molestias quis
+							suscipit sit blanditiis! Saepe minima ad provident consequuntur repudiandae architecto, totam inventore
+							nisi maiores reprehenderit repellat nobis itaque delectus?
+						</p>
+					</div>
+				</div>
+				<div>
+					<h1>number one</h1>
+					<div className={style.content}>
+						<img src={image} alt='' />
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti quaerat fugit ea, similique
+							explicabo. Aspernatur nemo qui nostrum, repellat quo delectus deserunt ipsa consectetur distinctio, cum
+							omnis fuga quisquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi molestias quis
+							suscipit sit blanditiis! Saepe minima ad provident consequuntur repudiandae architecto, totam inventore
+							nisi maiores reprehenderit repellat nobis itaque delectus?
+						</p>
+					</div>
+				</div>
+				<div>
+					<h1>number one</h1>
+					<div className={style.content}>
+						<img src={image} alt='' />
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti quaerat fugit ea, similique
+							explicabo. Aspernatur nemo qui nostrum, repellat quo delectus deserunt ipsa consectetur distinctio, cum
+							omnis fuga quisquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi molestias quis
+							suscipit sit blanditiis! Saepe minima ad provident consequuntur repudiandae architecto, totam inventore
+							nisi maiores reprehenderit repellat nobis itaque delectus?
+						</p>
+					</div>
+				</div>
+				<div>
+					<h1>number one</h1>
+					<div className={style.content}>
+						<img src={image} alt='' />
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti quaerat fugit ea, similique
+							explicabo. Aspernatur nemo qui nostrum, repellat quo delectus deserunt ipsa consectetur distinctio, cum
+							omnis fuga quisquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi molestias quis
+							suscipit sit blanditiis! Saepe minima ad provident consequuntur repudiandae architecto, totam inventore
+							nisi maiores reprehenderit repellat nobis itaque delectus?
+						</p>
+					</div>
+				</div>
+				<div>
+					<h1>number one</h1>
+					<div className={style.content}>
+						<img src={image} alt='' />
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti quaerat fugit ea, similique
+							explicabo. Aspernatur nemo qui nostrum, repellat quo delectus deserunt ipsa consectetur distinctio, cum
+							omnis fuga quisquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi molestias quis
+							suscipit sit blanditiis! Saepe minima ad provident consequuntur repudiandae architecto, totam inventore
+							nisi maiores reprehenderit repellat nobis itaque delectus?
+						</p>
+					</div>
+				</div>
+				<div>
+					<h1>number one</h1>
+					<div className={style.content}>
+						<img src={image} alt='' />
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti quaerat fugit ea, similique
+							explicabo. Aspernatur nemo qui nostrum, repellat quo delectus deserunt ipsa consectetur distinctio, cum
+							omnis fuga quisquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi molestias quis
+							suscipit sit blanditiis! Saepe minima ad provident consequuntur repudiandae architecto, totam inventore
+							nisi maiores reprehenderit repellat nobis itaque delectus?
+						</p>
+					</div>
+				</div>
+			</Slider>
+		</div>
 	);
 };
 
 export default Clothes;
+
+const SampleNextArrow = props => {
+	const { className, style, onClick } = props;
+	return (
+		<div className='arrow'>
+			<button onClick={onClick}>{'>>'}</button>
+		</div>
+	);
+};
